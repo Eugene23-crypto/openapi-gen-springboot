a9eacbaae7e289ecb24bb3f94d9aab2088bb39cc
eugene <eugenesena23@gmail.com>
2024-04-04 15:20:03 +0000

    First

---
+++ src/test/java/com/openapi/gen/example/openapigenspringboot/OpenapiGenSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openapi.gen.example.openapigenspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiGenSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
